f8c3325a8993555c7acae2ca5da2ef9057fad948
Hoai Phuong Vi <hoaiphuong070198@gmail.com>
2020-12-29 23:46:14 +0700

    Phuong push TagAnimal

--- src/components/tagAnimal.js
+++ src/components/tagAnimal.js
@@ -3,6 +3,7 @@ import { PlusOutlined, CheckOutlined} from '@ant-design/icons';
 import React, { useState, useEffect } from "react";
 import './style.css';
 import {apiFollowTag, apiUnfollowTag } from '../api/api'
+import classes from '../data/classes.json'
 export const TagAnimal = (props) => {
     const [follow, setFollow] = useState(props.isFollow)
     const [idfollow, setIdfollow] = useState(props.idFollow)
@@ -29,18 +30,20 @@ export const TagAnimal = (props) => {
     }
 
     return(
-        <div className= "d-inline-flex mr-5 mb-5 p-0" style={{width: "25%"}}>
+        <div className= "d-inline-flex mr-5 mb-5 p-0 col-md-4">
             <div>
-                <img src = "../images/avt1.jpg" style={{width: "90%"}}/>
+                <img src = {props.img} style={{width: "90%"}}/>
             </div>
             <div className="p-0">
-                <h4>Bò sát</h4>
+                <h6>{props.name}</h6>
                 {
                     follow ? 
                     <Button type="primary" onClick={handleUnfollowAnimal} icon ={<CheckOutlined />}>Following</Button> :
                     <Button type="primary" onClick={handleFollowAnimal} ghost icon ={<PlusOutlined />}>Follow</Button> 
                 }    
             </div>
+           
+
         </div>
     
     );
